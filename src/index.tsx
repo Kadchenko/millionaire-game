1169a1b7f72e2d4104e9da8a774815ef89b96526
kadchenko <dmitry.kadchenko@gmail.com>
2023-10-10 11:41:18 +0300

    add persisted state

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from '@store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '@store';
 
 import ErrorBoundary from '@components/ErrorBoundary';
 
@@ -13,9 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <PersistGate loading={null} persistor={persistor}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import gameReducer from './game/slice';
 import uiReducer from './ui/slice';
 
-export const store = configureStore({
-  reducer: {
-    game: gameReducer,
-    ui: uiReducer,
-  },
+const combinedReducers = combineReducers({
+  game: gameReducer,
+  ui: uiReducer,
 });
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
